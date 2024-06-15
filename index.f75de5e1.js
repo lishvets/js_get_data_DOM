81193b29444de20d8b6a71989c61ae089481d90b
Lidiia Shvets <lepoki051003@gmail.com>
2024-06-15 17:07:47 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const population = [
+    ...document.querySelectorAll(".population")
+];
+const totalPopulation = document.querySelector(".total-population");
+const averagePopulation = document.querySelector(".average-population");
+const total = population.map((el)=>+el.textContent.split(",").join("")).reduce((sum, num)=>sum + num, 0);
+const average = total / population.length;
+totalPopulation.textContent = total.toLocaleString("en-US");
+averagePopulation.textContent = average.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
